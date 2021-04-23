c4c32ad9be55bb4aef6ab61d607fae38b6726ff2
Martín Nicolás Precelle <77676733+MPrecelle@users.noreply.github.com>
2021-04-23 18:26:29 -0300

    Delete animate.js

--- animate.js
+++
@@ -1,9 +0,0 @@
-//  EFECTO APARECE/DESAPARECE DE COSTO MENSUAL UNA SOLA VEZ Y QUEDA EN PANTALLA
-
-const apareceDesaparece = () => {
-    $("#costoMensual").fadeToggle(1500, () => {
-        $("#costoMensual").fadeToggle(1500)
-    })
-}
-
-apareceDesaparece()
