0fe4bfa5dc6fcc4d274179a993f4c7e479af9c19
Martín Nicolás Precelle <77676733+MPrecelle@users.noreply.github.com>
2021-04-23 18:28:44 -0300

    Tercera entrega proyecto corregida

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 const URLJSON = "../../planes.json"
+let details = JSON.parse(localStorage.getItem("Plan elegido"))
 
  // IMPRIMO CARDS DE PLANES CON AJAX
 
@@ -29,8 +30,36 @@ const guardoPlan = (id) => {
             if (estado == "success") {
                 let datosCards = respuesta
                 let resultado = datosCards.find(p => p.planid == id)
-                localStorage.setItem("Plan elegido", JSON.stringify(resultado))}
+                localStorage.setItem("Plan elegido", JSON.stringify(resultado))
+                planSelected()
+            }
     })
  }
+
+ //OCULTO HTML VIEJO Y MUESTRO EL NUEVO UNA VEZ SELECCIONADO EL PLAN PARA CHECKOUT
+
+const planSelected = () => {
+    //IMPRIMO HTML CON LOS DATOS TOMADOS DESDE LOCALSTORAGE
+    $("#planDetails").append(`<div class="box">
+                                <h3>${details.nombre}</h3>
+                                <h4>$ ${details.precio}<span>[única vez]</span></h4>
+                                <ul class="text-center">
+                                <li class=" pricing__box--list"><i class="bx bx-check"></i>${details.infoA}</li>
+                                <li class=" pricing__box--list"><i class="bx bx-check"></i>${details.infoB}</li>
+                                <li class=" pricing__box--list"><i class="bx bx-check"></i>${details.infoC}</li>
+                                </ul>
+                                <button onclick="confirmoPlan()" id="confirmButtonPlan" class="get-started-btn">CONFIRMAR</a>
+                            </div>`)
+    //OCULTO HTML EXISTENTE CON FADEOUT
+    $("#cardsPlanes").fadeOut(300)
+    //IMPRIMO NUEVO HTML CON FADEIN
+    $("#generalDetails").fadeIn(300)
+}
+
+const confirmoPlan = () => {
+    $("#costoMensual").fadeOut(300)
+    $("#generalDetails").fadeOut(300)
+    $("#successConfirmPlan").fadeIn(300)
+}
 
  
